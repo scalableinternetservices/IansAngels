30609aee97081d71b5a6ed6b4a1cad17f85e96d1
Rhys <rhystracy@ucsb.edu>
2022-11-02 23:12:51 -0700

    rt - added navbar to pos and home pages, added bg image for home page

--- iansangels_frontend/pages/index.js
+++ iansangels_frontend/pages/index.js
@@ -6,13 +6,32 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import bg_image from "../public/food_bg.jpg"; 
 
 
 export default function Home() {
   return (
     <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Ian's Angels All-In-One Restaurant WebApp</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/client">Client</Nav.Link>
+              <Nav.Link href="/pos">POS</Nav.Link>
+              <Nav.Link href="/kitchen">Kitchen</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div style={{backgroundImage: `url(${bg_image.src})`, backgroundRepeat:"no-repeat", backgroundSize:"contain", height:1000, width:"auto", overflow:"auto"}}>
       <div className="m-5">
-        <h1 className="text-center">Home Page</h1>
+        <h1 className="text-center">Welcome to Ian's Angels All-In-One Restaurant WebApp</h1>
       </div>
       <div className="m-5">
         <Container>
@@ -34,6 +53,7 @@ export default function Home() {
             </Col>
           </Row>
         </Container>
+      </div>
       </div>
     </div>
   )
