a4a038f71a27868ce3d271daa0026761e5b1ec10
Erwan Fraisse <efraisse@ucsb.edu>
2022-11-19 19:46:29 -0800

    added inventory restrictions to orders create function. Now, whenever an order is created, the backend can successfully remove the ingredients required to make it from the database. It can also detect if there aren't enough ingredients to make the current order and just removes it automatically. If a user also currently has an order, it will not send another order and instead ask the user to update their current order. Still need to make sure that when updating an order that ingredients are modified though. Will probably stay on this branch until this has been accomplished

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,8 +18,58 @@ class OrdersController < ApplicationController
             return
         end
 
+        currentOrder = Order.find_by(person: personData)
+
+        if currentOrder != nil
+            print "you already have a current order scheduled, please just update your current order!"
+            return
+        end
+
         order = Order.new(order_params)
         order.person = personData
+
+        #need to make sure that there are enough ingredients to use for the order
+        ordersToDelete = Array.new
+
+        for item in order.itemNames
+            curOrder = Menu.find_by(itemName: item)
+            
+            for ingredient in curOrder.ingredients
+                quantityAndFoodName = ingredient.split(':', -1)
+                quantityAndFoodName[0].strip!
+                quantityAndFoodName[1].strip!
+                qty = quantityAndFoodName[0].to_i
+                food = quantityAndFoodName[1]
+
+                inventoryItem = Inventory.find_by(foodName: food)
+                inventoryFood = inventoryItem.foodName
+                inventoryAmount = inventoryItem.quantity
+                if inventoryAmount - qty >= 0
+                    #can order
+                    inventoryItem.quantity -= qty
+
+                    puts "\nCHECK VALUE BELOW\n"
+                    puts inventoryItem.foodName
+                    puts inventoryItem.quantity
+                    puts "CHECK VALUE ABOVE\n"
+
+                    if inventoryItem.update(inventory_params)
+                        print "successfully updated " + inventoryItem.foodName
+                    else
+                        print "did not successfully update " + inventoryItem.foodName
+                    end
+                else
+                    #can't order
+                    print "Sorry, there are not enough ingredients at the moment to order this item"
+                    ordersToDelete.push(item)
+                    break
+                end
+            end
+        end
+
+        for item in ordersToDelete
+            order.itemNames.delete(item)
+        end
 
         if order.save
             render json: OrderSerializer.new(order).serialized_json
@@ -67,5 +117,9 @@ class OrdersController < ApplicationController
 
     def person_params
         params.permit(:username, :password, :email, :position, :completedOrders => [[]])
+    end
+
+    def inventory_params
+        params.permit(:foodName, :quantity)
     end
 end
